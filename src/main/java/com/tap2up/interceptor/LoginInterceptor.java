46b9273c3c73a9c56a8e7c9f362e3b01921b0d19
414507259 <414507259@qq.com>
2019-08-09 14:52:35 +0800

    Initial commit

---
+++ src/main/java/com/tap2up/interceptor/LoginInterceptor.java
@@ -0,0 +1,11 @@
+package com.tap2up.interceptor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 张雪彬
+ * Date: 2019/08/09 14:34
+ * Description:
+ * Version: V1.0
+ */
+public class LoginInterceptor {
+}
