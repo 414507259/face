c78a5de7c754e9fef78a2c97a0f727d465a65ce8
han <qh95918@foxmail.com>
2019-09-16 10:07:40 +0800

    han

--- src/main/java/com/tap2up/controller/UserInfoController.java
+++ src/main/java/com/tap2up/controller/UserInfoController.java
@@ -7,9 +7,7 @@ import com.tap2up.service.AlfService;
 import com.tap2up.service.UserInfoService;
 import com.tap2up.utils.IdCardUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
